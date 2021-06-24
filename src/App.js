af1fb5d3d67ace73b6f2bacf87dded4347866d75
Lirca Jimenez <lirca.je@gmail.com>
2021-06-24 16:00:26 -0400

    changed to functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import Navbar from "./components/layout/Navbar.js"
-import UserItem from "./components/users/UserItem.js"
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar.js";
+// import UserItem from "./components/users/UserItem.js";
+import Users from "./components/users/Users.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
-    );    
+    );
   }
 }
 
