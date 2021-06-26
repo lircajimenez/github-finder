def2a1afb30ce3b110f2d66ed2155eed5a5d9b9c
Lirca Jimenez <lirca.je@gmail.com>
2021-06-25 21:50:37 -0400

    HTTP request, spinner component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar.js";
 // import UserItem from "./components/users/UserItem.js";
 import Users from "./components/users/Users.js";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const response = await axios.get("https://api.github.com/users");
+    console.log("response", response.data);
+    this.setState({ users: response.data, loading: false });
+  }
+
+  // ---> version w/out async/await <---
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/users")
+  //     .then((response) => console.log("response", response.data));
+  // }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
